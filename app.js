73035792f7e7bdbc0cf2278b7e3f48a69beebb88
hypnosis <dima@kurzepost.de>
2014-07-01 02:31:56 +0400

    added metrika and analytics

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var http = require('http');
 var url = require('url');
 var request = require('request');
+var qs = require('qs');
+var yametrika = require('yametrika');
+var ua = require('universal-analytics');
+
  
 http.createServer(function(req, res){
 	var requestURL = url.parse(req.url, true)
@@ -10,12 +14,26 @@ http.createServer(function(req, res){
 		var imgBinary = new Buffer(imgHex, 'hex');
 		res.writeHead(200, {'Content-Type': 'image/gif' });
 		res.end(imgBinary, 'binary');
+
+		var queryParam = qs.parse(requestURL.query);
+		var YaParam = queryParam.yaparam;
+		var GaParam = queryParam.gaparam;
+		// console.log('queryParam',queryParam);
 		
-		var trackUrl = requestURL.query && requestURL.query.track ? requestURL.query.track : null;
-	
-		if (trackUrl) {
-			request.post(trackUrl);
-		}	
+
+		if (YaParam && YaParam.id && YaParam.reachGoal) {
+			var yaCounter = yametrika.counter({id: YaParam.id});
+			yaCounter.req(req);
+			yaCounter.reachGoal(YaParam.reachGoal);			
+		}
+
+		if (GaParam && GaParam.id && GaParam.category && GaParam.action) {
+			var gaCounter = ua(GaParam.id);
+			gaCounter.event(GaParam.category,GaParam.action, GaParam.label, GaParam.value).send();
+
+		}
+		
+		
 	} else { 
 		res.writeHead(400, {'Content-Type': 'text/plain' });
 		res.end('');
